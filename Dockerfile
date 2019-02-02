955481e94db69ecdaebc1755dc060abf736c02e3
Javier Lopez Lopez <sjavierlopez@gmail.com>
2019-02-02 13:00:31 -0600

    Add documentation in README.md file

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,16 @@ FROM golang:alpine AS builder
 
 LABEL maintainer="Javier Lopez Lopez<sjavierlopez@gmail.com>"
 
+ENV GOUSER_HOME    /home/gouser/
+ARG AWS_ACCESS_KEY_ID=xxx
+ARG AWS_SECRET_ACCESS_KEY=xxx
+ARG user=gouser
+ARG group=gouser
+ARG uid=1000
+ARG gid=1000
 
 RUN apk update \
-    && apk add --no-cache git \
+    && apk --update --no-cache add bash  git \
     && mkdir -p ${GOPATH}/src ${GOPATH}/bin
 
 
@@ -13,30 +20,19 @@ COPY . .
 
 RUN cd ${GOPATH}/src/app-platform/cmd \ 
     && go get -d -v ./... \ 
-    && go build -o ${GOPATH}/bin/orquesta ${GOPATH}/src/app-platform/cmd/orquesta/main.go \
-    && go build -o ${GOPATH}/bin/deploy ${GOPATH}/src/app-platform/cmd/deploy/main.go
+    && go build -o /usr/bin/orquesta ${GOPATH}/src/app-platform/cmd/orquesta/main.go \
+    && go build -o /usr/bin/deploy ${GOPATH}/src/app-platform/cmd/deploy/main.go \
+    && mkdir -p $(dirname $GOUSER_HOME) \
+    && mkdir -p /home/gouser/.aws/
 
-RUN adduser -D -g '' gouser    
+RUN addgroup -g ${gid} ${group}
+RUN adduser -h ${GOUSER_HOME} -S ${user} -u ${uid} -G ${group} -s /bin/bash ${user} 
 
-FROM ubuntu
-
-ENV GOUSER_HOME          /home/gouser/
-
-ARG user=gouser
-ARG group=gouser
-ARG uid=1000
-ARG gid=1000
-
-RUN mkdir -p $(dirname $GOUSER_HOME) \
-    && groupadd -g ${gid} ${group} \
-    && useradd -d "$GOUSER_HOME" -u ${uid} -g ${gid} -m -s /bin/bash ${user}
-
-COPY --from=builder /go/bin/deploy  /usr/bin/deploy
-COPY --from=builder /go/bin/orquesta  /usr/bin/orquesta
-COPY config/aws/config  /home/gouser/.aws/config
-COPY config/aws/credentials  /home/gouser/.aws/credentials
+RUN touch /home/gouser/.aws/credentials \
+    && echo "[default]" >> /home/gouser/.aws/credentials \
+    && echo "aws_access_key_id = ${AWS_ACCESS_KEY_ID}" >> /home/gouser/.aws/credentials \
+    && echo "aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" >> /home/gouser/.aws/credentials
 
 RUN chown -R gouser:gouser  /home/gouser/
-
 
 USER gouser
--- cmd/orquesta/main.go
+++ cmd/orquesta/main.go
@@ -4,42 +4,45 @@ import (
 	"app-platform/internals/aws"
 	"flag"
 	"fmt"
-	"os"
 )
 
 func main() {
 
 	ami := flag.String("ami", "ami-0ca6dfcefdde35d25", "You need to set the AMI to start the deployment")
 	region := flag.String("region", "us-east-1", "Region")
+	vpc := flag.String("vpc", "vpc-d69537ac", "Vpc id")
 	instanceType := flag.String("instance-type", "t2.micro", "Instance Type")
 	elbName := flag.String("elb", "app-platform", "Elastic Load Balancer")
+	sgName := flag.String("security-group-name", "app-platform", "Elastic Load Balancer")
 	launchConfigName := flag.String("launch-config-name", "app-platform", "Launch Config Name")
 	cidr := flag.String("cidr", "172.31.0.0/20", "CIDR block")
+	autoScalingName := flag.String("autoscaling-group-name", "app-platform", "Name of the Autoscaling Group")
+	deploy := flag.String("deploy", "plan", "Type of the execution")
 	flag.Parse()
 
 	const MIN_INSTANCES_PER_GROUP = 2
 	const MAX_INSTANCES_PER_GROUP = 4
 
-	if *ami == "" {
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
-
 	aws.PrintMessage("\n---------------------------------------")
 	aws.PrintMessage("Orquesta App has been started.........")
 	aws.PrintMessage("Building blocks using AMI:" + *ami)
+
+	if *deploy == "plan" {
+		aws.PrintMessage("-------- MODE: TEST -----------------")
+		return
+	}
 
 	ob := &aws.OrchestratorBuilder{}
 	awsServiceMangment := ob.
 		NewSession(*region).
 		EnableEc2().
-		CreateVpc().
+		CreateVpc(*vpc).
 		CreateSubnet(*cidr).
 		CreateLoadBalancer(*elbName, 80, 80).
-		CreateSecurityGroupConfiguration("app-platform", "app platform security group", ob.Orchestrator.VpcID).
+		CreateSecurityGroupConfiguration(*sgName, "app platform security group", ob.Orchestrator.VpcID).
 		InputSecurityRule(ob.Orchestrator.SecurityGroup.GroupName).
 		CreateLaunchConfiguration(*ami, *instanceType, *launchConfigName).
-		DeployAutoScalingGroup("app-platform", MIN_INSTANCES_PER_GROUP, MAX_INSTANCES_PER_GROUP).
+		DeployAutoScalingGroup(*autoScalingName, MIN_INSTANCES_PER_GROUP, MAX_INSTANCES_PER_GROUP).
 		Build()
 
 	fmt.Println("\n---------- summary ----------")
